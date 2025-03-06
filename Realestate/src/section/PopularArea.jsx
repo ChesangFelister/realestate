1b87a6f8194b0d04b81d64f6adf8802fce9a777b
ChesangFelister <chesangfelister@gmail.com>
2025-03-06 12:03:44 +0300

    properties

--- Realestate/src/section/PopularArea.jsx
+++ Realestate/src/section/PopularArea.jsx
@@ -1,11 +1,114 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "aos/dist/aos.css";
+import AOS from "aos";
+import "../index.css";
+import area1 from "../assets/images/area1.jpg";
+import area2 from "../assets/images/area2.jpg";
+import area3 from "../assets/images/area3.jpg";
+import { useDarkMode } from "../components/DarkModeContext";
 
 const PopularArea = () => {
-  return (
-    <div>
-      PopularArea
-    </div>
-  )
-}
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-out",
+      delay: 100,
+    });
+  }, []);
 
-export default PopularArea
+  const { darkMode } = useDarkMode();
+
+  return (
+    <div className={`${darkMode ? "dark bg-black" : "light bg-transparent"}`}>
+      <section
+        className={`${
+          darkMode ? "dark bg-gray-800" : "light bg-red-100"
+        } lg:w-[90%] h-fit w-full m-auto bg-cover bg-center rounded-xl flex justify-center flex-col items-center lg:px-20 py-20 px-6`}
+      >
+        <div
+          id="top"
+          className="w-full grid lg:grid-cols-3 grid-cols-1 justify-center items-center gap-8"
+        >
+          <div>
+            <h1 data-aos="zoom-in" className="text-red-500 capitalize">
+              Popular Areas
+            </h1>
+            <h1
+              data-aos="zoom-in"
+              className="text-black text-[40px] font-semibold leading-10 mt-4 "
+              // dark:text-white
+            >
+              explore more
+              <br />
+              Popular areas
+            </h1>
+          </div>
+          <div className="grid lg:grid-cols-3 col-span-2 grid-cols-1 justify-center items-center gap-6">
+            <div
+              data-aos="zoom-in"
+              data-aos-delay="400"
+              style={{ backgroundImage: `url(${area1})` }}
+              className="h-[400px] bg-cover bg-center rounded-xl"
+            ></div>
+            <div
+              data-aos="zoom-in"
+              data-aos-delay="400"
+              style={{ backgroundImage: `url(${area2})` }}
+              className="h-[400px] bg-cover bg-center rounded-xl"
+            ></div>
+            <div
+              data-aos="zoom-in"
+              data-aos-delay="400"
+              style={{ backgroundImage: `url(${area3})` }}
+              className="h-[400px] bg-cover bg-center rounded-xl"
+            ></div>
+          </div>
+        </div>
+        <div
+          id="bottom"
+          className="w-full grid lg:grid-cols-3 grid-cols-1 lg:justify-center justify-start items-center gap-6"
+        >
+          <div
+            data-aos="slide-up"
+            data-aos-delay="200"
+            className="flex justify-center lg:items-center w-full"
+          >
+            <h1 className="text-black text-7xl font-semibold dark:text-white">
+              5k
+            </h1>
+            <h1>
+              ACTIVE<br></br>LISTINGS
+            </h1>
+          </div>
+          <div
+            data-aos="slide-up"
+            data-aos-delay="200"
+            className="flex justify-center lg:items-center w-full"
+          >
+            <h1 className="text-black text-7xl font-semibold dark:text-white">
+              5k
+            </h1>
+            <h1>
+              ACTIVE<br></br>LISTINGS
+            </h1>
+          </div>
+          <div
+            data-aos="slide-up"
+            data-aos-delay="200"
+            className="flex justify-center lg:items-center w-full"
+          >
+            <h1 className="text-black text-7xl font-semibold dark:text-white">
+              5k
+            </h1>
+            <h1>
+              ACTIVE<br></br>LISTINGS
+            </h1>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default PopularArea;
--- Realestate/src/section/Properties.jsx
+++ Realestate/src/section/Properties.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import "aos/dist/aos.css";
+import AOS from "aos";
+import "../index.css";
+import { FaBath, FaShare, FaBed, FaUserCircle, FaPlus, FaMapMarkerAlt, FaVideo, FaCamera } from "react-icons/fa";
+import { property } from "../components/export.js";
+import { MdSpaceDashboard } from "react-icons/md";
+import { useDarkMode } from "../components/DarkModeContext";
 
 const Properties = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-out",
+      delay: 100,
+    });
+  }, []);
+
+  const { darkMode } = useDarkMode();
+
   return (
-    <div>
-      Properties
+    <div className={`${darkMode ? "dark bg-black" : "light bg-transparent"}`}>
+      <section id="properties" className="lg:w-[90%] w-full m-auto lg:px-20 flex justify-center flex-col items-start py-20 px-6 gap-10">
+        <div className="flex flex-col justify-center items-start gap-4">
+          <h1 data-aos="zoom-in" className='text-red-500 dark:text-white'>PROPERTIES</h1>
+          <h1 data-aos="zoom-in" className="text-black text-4xl font-semibold dark:text-white">
+            Explore the latest properties
+          </h1>
+        </div>
+        <div id="grid-box" className="w-full grid lg:grid-cols-3 grid-cols-1 justify-center items-center gap-8">
+          {property.map((item, index) => (
+            <div data-aos="zoom-in" data-aos-delay="200" key={index} className="w-full bg-white dark:bg-gray-800 rounded-xl">
+              <div id="image-box" className="bg-cover bg-center h-[250px] rounded-t-xl p-4 flex flex-col justify-between items-end" style={{ backgroundImage: `url(${item.images})` }}>
+                <div className="flex gap-2">
+                  <button className="bg-white p-2 rounded-full hover:bg-red-500 hover:text-white">
+                    <FaShare />
+                  </button>
+                  <button className="bg-white p-2 rounded-full hover:bg-red-500 hover:text-white">
+                    <FaPlus />
+                  </button>
+                </div>
+                <div className="w-full flex justify-between items-center">
+                  <span className="bg-white px-4 py-1 rounded-full text-sm">{item.type}</span>
+                  <span className="bg-red-500 text-white px-4 py-1 rounded-full text-sm">{item.status}</span>
+                </div>
+              </div>
+              <div className="p-4 flex flex-col gap-3">
+                <div className="flex items-center gap-2">
+                  <FaMapMarkerAlt className="text-red-500" />
+                  <p className="text-gray-600 dark:text-gray-300">{item.location}</p>
+                </div>
+                <h1 className="text-xl font-semibold dark:text-white">{item.name}</h1>
+                <div className="flex justify-between items-center">
+                  <div className="flex items-center gap-2">
+                    <FaBed className="text-red-500" />
+                    <p className="text-gray-600 dark:text-gray-300">{item.beds} Beds</p>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <FaBath className="text-red-500" />
+                    <p className="text-gray-600 dark:text-gray-300">{item.baths} Baths</p>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <MdSpaceDashboard className="text-red-500" />
+                    <p className="text-gray-600 dark:text-gray-300">{item.area} sqft</p>
+                  </div>
+                </div>
+                <div className="flex justify-between items-center pt-3 border-t">
+                  <h1 className="text-xl font-semibold text-red-500">${item.price}</h1>
+                  <div className="flex items-center gap-2">
+                    <FaUserCircle className="text-2xl text-gray-500" />
+                    <p className="text-gray-600 dark:text-gray-300">{item.agent}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   )
 }
